625586e259f1b6440f55f4895cb62c4cb79d7824
Atiqa Lohani <atiqatariqkhanlohani@gmail.com>
2026-08-31 21:19:57 +0500

    swagger.js

--- nexus-api-core/src/config/swagger.js
+++ nexus-api-core/src/config/swagger.js
@@ -1,1 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Nexus API Core',
+      version: '1.0.0',
+      description: 'Industrial-grade Backend RESTful API Core'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api/v1',
+        description: 'Development Server'
+      }
+    ]
+  },
+  apis: ['./src/routes/*.js']
+};
+
+module.exports = swaggerJSDoc(options);
